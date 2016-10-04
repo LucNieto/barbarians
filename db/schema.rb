4446f2a3934d25ed91ce5bba5949f11ed81b39c8
Uziel Trujillo <uziel.trujillo@gmail.com>
2016-10-03 23:47:04 -0500

    [#131107723] Solucionando algunos errores de diseño
    Nota:
    - El partial se coloca donde va
    - Se agregan botones de eliminar en las vistas editar y mostrar
    - Solucionando problema con la tabla schema.rb
    	modified:   app/views/admin/categories/_form.html.erb
    	modified:   app/views/admin/categories/edit.html.erb
    	modified:   app/views/admin/categories/new.html.erb
    	modified:   app/views/admin/categories/show.html.erb
    	modified:   app/views/layouts/application.html.erb
    	modified:   config/application.yml
    	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160926025916) do
 
-ActiveRecord::Schema.define(version: 20161002041404) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,13 +20,6 @@ ActiveRecord::Schema.define(version: 20161002041404) do
     t.string   "nombre_categoria"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-  end
-
-
-  create_table "labels", force: :cascade do |t|
-    t.string   "nombre_de_etiqueta"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
   end
 
 end
