56acee139efd842cf5e7f7989c9a84354a1087fc
Luc <noodle3030@gmail.com>
2016-10-04 11:36:30 -0500

    modelo y controlador creado de labels

---
+++ app/controllers/admin/labels_controller.rb
@@ -0,0 +1,50 @@
+module Admin
+  class LabelsController < ApplicationController
+      before_action :set_label, only: [:edit, :show, :update, :destroy]
+    def index
+      @labels = Label.all
+    end
+
+    def show
+    end
+
+    def new
+      @label = Label.new
+    end
+
+    def create
+      @label = Label.new(label_params)
+      if @label.save
+        redirect_to admin_labels_path, :notice => "Etiqueta creada"
+      else
+        render "new"
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @label.update_attributes(label_params)
+        redirect_to admin_labels_path, :notice => "Etiqueta actualizada"
+      else
+        render "edit"
+      end
+    end
+
+    def destroy
+      @label.destroy
+      redirect_to admin_labels_path, :notice => "Etiqueta borrada"
+    end
+
+    private
+      def label_params
+        params.require(:label).permit(:nombre_de_eqiqueta)
+      end
+
+      def set_label
+        @label = Label.find(params[:id])
+      end
+    end
+end
+
