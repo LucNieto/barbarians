93c167c002a1d0bbee4bfc7d0ff4b79257240dcd
Luc <noodle3030@gmail.com>
2016-10-06 15:55:43 -0500

    modified:   app/controllers/application_controller.rb 	new file:   app/models/user.rb 	modified:   app/views/home/index.html.erb 	modified:   app/views/layouts/application.html.erb 	new file:   app/views/users/confirmations/new.html.erb 	new file:   app/views/users/mailer/confirmation_instructions.html.erb 	new file:   app/views/users/mailer/password_change.html.erb 	new file:   app/views/users/mailer/reset_password_instructions.html.erb 	new file:   app/views/users/mailer/unlock_instructions.html.erb 	new file:   app/views/users/passwords/edit.html.erb 	new file:   app/views/users/passwords/new.html.erb 	new file:   app/views/users/registrations/edit.html.erb 	new file:   app/views/users/registrations/new.html.erb 	new file:   app/views/users/sessions/new.html.erb 	new file:   app/views/users/shared/_links.html.erb 	new file:   app/views/users/unlocks/new.html.erb 	modified:   config/environments/development.rb 	new file:   config/initializers/devise.rb 	new file:   config/locales/devise.en.yml 	modified:   config/routes.rb 	new file:   db/migrate/20161004205441_devise_create_users.rb 	new file:   db/migrate/20161006184920_add_name_to_users.rb 	modified:   db/schema.rb 	new file:   test/fixtures/users.yml 	new file:   test/models/user_test.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+ before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :telefono, :is_female, :email, :password, :date_of_birth ])
+            devise_parameter_sanitizer.permit(:sign_in, keys: [:nombre, :telefono, :is_female, :email, :password, :date_of_birth])
+        end
+
 end
