c64d8f2498755b38ee4e4789c6d3e87a7bb280f6
Luc <noodle3030@gmail.com>
2016-10-13 13:41:08 -0500

    Actualizando

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
