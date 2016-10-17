aa750a34b1c7c96cd2b24fc08e8f460513d27b1e
Luc <noodle3030@gmail.com>
2016-10-17 10:40:28 -0500

    [#131394227]

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,51 +1,52 @@
 module Admin
-  class CategoriesController < ApplicationController
-      before_action :set_category, only: [:edit, :show, :update, :destroy]
-    http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
-    def index
-      @categories = Category.all
-    end
-
-    def show
-    end
-
-    def new
-      @category = Category.new
-    end
-
-    def create
-      @category = Category.new(category_params)
-      if @category.save
-        redirect_to admin_categories_path, :notice => "categoria creada"
-      else
-        render "new"
-      end
-    end
-
-    def edit
-    end
-
-    def update
-      if @category.update_attributes(category_params)
-        redirect_to admin_categories_path, :notice => "Categoria actualizada"
-      else
-        render "edit"
-      end
-    end
-
-    def destroy
-      @category.destroy
-      redirect_to admin_categories_path, :notice => "Categoria borrada"
-    end
-
-    private
-      def category_params
-        params.require(:category).permit(:nombre_categoria)
+    class CategoriesController < ApplicationController
+        before_action :set_category, only: [:edit, :show, :update, :destroy]
+        before_filter :authenticate_users!
+        #http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
+      def index
+        @categories = Category.all
       end
 
-      def set_category
-        @category = Category.find(params[:id])
+      def show
       end
+
+      def new
+        @category = Category.new
+      end
+
+      def create
+        @category = Category.new(category_params)
+        if @category.save
+          redirect_to admin_categories_path, :notice => "categoria creada"
+        else
+          render "new"
+        end
+      end
+
+      def edit
+      end
+
+      def update
+        if @category.update_attributes(category_params)
+          redirect_to admin_categories_path, :notice => "Categoria actualizada"
+        else
+          render "edit"
+        end
+      end
+
+      def destroy
+        @category.destroy
+        redirect_to admin_categories_path, :notice => "Categoria borrada"
+      end
+
+      private
+        def category_params
+          params.require(:category).permit(:nombre_categoria)
+        end
+
+        def set_category
+          @category = Category.find(params[:id])
+        end
     end
 end
 
