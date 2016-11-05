9305c65f1bccd1528765ae36f16ec732273fa28b
rztap <rz.tap@outlook.com>
2016-11-05 16:36:52 -0600

    Revision Slider Json 	new file:   app/assets/javascripts/pgstatics.coffee 	new file:   app/assets/javascripts/products.coffee 	new file:   app/assets/stylesheets/index.css 	modified:   app/assets/stylesheets/navbar.css 	new file:   app/assets/stylesheets/pgstatics.scss 	new file:   app/assets/stylesheets/products.scss 	new file:   app/controllers/pgstatics_controller.rb 	new file:   app/controllers/products_controller.rb 	new file:   app/helpers/pgstatics_helper.rb 	new file:   app/helpers/products_helper.rb 	modified:   app/views/home/_slider.html.erb 	modified:   app/views/home/index.html.erb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/layouts/template/_navbar.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'pgstatics/aboutus'
+
+  get 'pgstatics/contacts'
+
+  get 'pgstatics/courses'
+
+  get 'products/index'
+
   devise_for :users
   namespace :admin do
     resources :labels
     resources :categories
   end
   root 'home#index'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161006184920) do
 
   # These are extensions that must be enabled in order to support this database
