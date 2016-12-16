c1d9fd3817f768e961f52e7a504346f7cb8151d1
Uziel Trujillo <uziel.trujillo@gmail.com>
2016-12-15 20:16:23 -0600

    Implementando cambios para el funcionamiento correcto del many to many

--- app/models/labeling.rb
+++ app/models/labeling.rb
@@ -1,5 +1,4 @@
 class Labeling < ApplicationRecord
-  belongs_to :product, :inverse_of => :labelings
-  belongs_to :label, :inverse_of => :labelings
-
+  belongs_to :product
+  belongs_to :label
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ApplicationRecord
-  attr_accessor :label_ids, :nombre_de_etiqueta
   belongs_to :category
   has_attached_file :image, styles: { medium: "450x450>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  #has_and_belongs_to_many :labels
-  has_many :labelings, :dependent => :destroy, :autosave => true , :inverse_of => :product
+  has_many :labelings, :dependent => :destroy
   has_many :labels, through: :labelings
 end
 
